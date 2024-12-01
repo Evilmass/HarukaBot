4aa7e5e27c3b50985ae6305cf33a8b28980aebdc
Evilmass <21218296+Evilmass@users.noreply.github.com>
2024-12-01 21:50:22 +0800

    cron job

--- haruka_bot/plugins/__init__.py
+++ haruka_bot/plugins/__init__.py
@@ -7,5 +7,5 @@ from .at import at_off, at_on  # noqa: F401
 from .dynamic import dynamic_off, dynamic_on  # noqa: F401
 from .live import live_now, live_off, live_on  # noqa: F401
 from .permission import permission_off, permission_on  # noqa: F401
-from .pusher import dynamic_pusher, live_pusher  # noqa: F401
+from .pusher import dynamic_pusher, live_duration, live_pusher  # noqa: F401
 from .sub import add_sub, delete_sub, sub_list  # noqa: F401
--- haruka_bot/plugins/pusher/live_duration.py
+++ haruka_bot/plugins/pusher/live_duration.py
@@ -11,3 +11,9 @@ async def notify_live_duration():
 @scheduler.scheduled_job("cron", hour=4, minute=0, second=0, id="reset_live_duration")
 async def reset_live_duration():
     await db.reset_live_duration()
+
+
+@scheduler.scheduled_job("cron", second="*/10", id="test")
+async def test():
+    # await db.reset_live_duration()
+    await get_live_duration(cron=True)
