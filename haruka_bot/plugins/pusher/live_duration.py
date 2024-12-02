84aa78b1832826a4d39a98164ac8c974ae8dd15d
Evilmass <21218296+Evilmass@users.noreply.github.com>
2024-12-02 19:27:50 +0800

    cronjob add  timezone

--- haruka_bot/plugins/pusher/live_duration.py
+++ haruka_bot/plugins/pusher/live_duration.py
@@ -1,20 +1,25 @@
+from typing import Optional
+
+from nonebot.adapters.onebot.v11.event import MessageEvent
+
 from ...database import DB as db
 from ...utils import scheduler
 from ..live_duration import get_live_duration
 
 
-@scheduler.scheduled_job("cron", hour=0, minute=0, second=0, id="get_live_duration")
+@scheduler.scheduled_job("cron", hour=0, minute=0, second=0, timezone="Asia/Shanghai", id="get_live_duration")
 async def notify_live_duration():
     await get_live_duration(cron=True)
 
 
-@scheduler.scheduled_job("cron", hour=4, minute=0, second=0, id="reset_live_duration")
+@scheduler.scheduled_job("cron", hour=4, minute=0, second=0, timezone="Asia/Shanghai", id="reset_live_duration")
 async def reset_live_duration():
     await db.reset_live_duration()
 
 
-@scheduler.scheduled_job("cron", second="*/10", id="test")
+@scheduler.scheduled_job("cron", second="*/5", timezone="Asia/Shanghai", id="test")
 async def test():
     # await db.reset_live_duration()
-    # await get_live_duration(cron=True)
+    # message = await get_live_duration(cron=True)
+    # print(message)
     pass
